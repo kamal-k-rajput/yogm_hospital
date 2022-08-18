60f898212ed1908693e5f39a0a07ea5b05edfed0
kamal-kishor-rajput <90085564+kamal-kishor-rajput@users.noreply.github.com>
2022-08-18 17:21:09 +0530

    added sitemap

--- src/components/Homepage/Contact/Contact.jsx
+++ src/components/Homepage/Contact/Contact.jsx
@@ -2,25 +2,20 @@ import React from "react";
 import { Link } from "react-router-dom";
 import GoToTop from "../../others/MoveToTop";
 import "./Contact.css";
+import { InPhotoText } from "../../ReUsableCode/InPhotoText";
 import Map from "./Map";
 export const Contact = () => {
   return (
     <div>
-      <section className="contact-us">
-        <div>
-          <div>CONTACT US</div>
-          <Link to="/">Home </Link>
-          {" > "}
-          <Link to="/contact">Contact</Link>
-        </div>
-      </section>
-      <div className="connect-us display-flex-horizontal">
-        <div>
-          <h3>Connect With Us</h3>
-          <p>Achhalda Road Bidhuna</p>
-          <p>Auraiya Uttar Pradesh 206243</p>
-          <p>9170597957, 9634946398</p>
-        </div>
+      <InPhotoText
+        props={{
+          name: "Contact Us",
+          link: "contact",
+          heading: "Contact Us",
+        }}
+      />
+
+      <div className="contact-us">
         <div>
           <h3>Connect With Us</h3>
           <p>Achhalda Road Bidhuna</p>
@@ -29,11 +24,14 @@ export const Contact = () => {
         </div>
         <div>
           <h3>Connect With Us</h3>
-          <p>Achhalda Road Bidhuna</p>
-          <p>Auraiya Uttar Pradesh 206243</p>
-          <p>9170597957, 9634946398</p>
+          <p>Kachora Road</p>
+          <p>Etwah Uttar Pradesh 206001</p>
+          <p>
+            <b>Under Construction</b>
+          </p>
         </div>
       </div>
+      <h3>We are happy to serve you.</h3>
       <div className="map">
         <Map props={{ height: "350px" }} />
       </div>
