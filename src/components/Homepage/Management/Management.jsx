7e69182d6c1f1095c5d08406c21e711de426c335
kamal-kishor-rajput <90085564+kamal-kishor-rajput@users.noreply.github.com>
2022-09-21 14:04:19 +0530

    change in management page

--- src/components/Homepage/Management/Management.jsx
+++ src/components/Homepage/Management/Management.jsx
@@ -63,20 +63,8 @@ export const Management = () => {
           );
         })}
       </div>
-      {
-        <div>
-          <ins
-            class="adsbygoogle"
-            style={"display:block"}
-            data-ad-client="ca-pub-2960092495542092"
-            data-ad-slot="1489967335"
-            data-ad-format="auto"
-            data-full-width-responsive="true"
-          ></ins>
-          <script>(adsbygoogle = window.adsbygoogle || []).push({});</script>
-        </div>
-      }
       <GoToTop />
     </>
   );
 };
+
