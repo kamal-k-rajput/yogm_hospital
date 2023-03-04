c5747efce56d80ff40640add5c63bb438eb12e3b
kamal-kishor-rajput <90085564+kamal-kishor-rajput@users.noreply.github.com>
2023-03-04 12:44:25 +0530

    remove un necessary space

--- src/index.js
+++ src/index.js
@@ -4,16 +4,13 @@ import App from "./App";
 import { Provider } from "react-redux";
 import store from "./store/store";
 import { HelmetProvider } from "react-helmet-async";
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-
-        <HelmetProvider>
-          <App />
-        </HelmetProvider>
-
+      <HelmetProvider>
+        <App />
+      </HelmetProvider>
     </Provider>
   </React.StrictMode>
 );
